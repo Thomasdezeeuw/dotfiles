194a19204e9dcc3e65ef1e4ad931193145dab656
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2018-06-05 23:05:21 +0200

    Cleanup install script
    This is a work in progress that prefers to use `.config` over the home
    directory. Currently only the git and i3 configurations are linked.
    
    The creation of directories also changed from a single source directory
    per language to a single source directory for all languages. The
    language used shouldn't really matter.

--- install.sh
+++ install.sh
@@ -4,65 +4,46 @@
 # # Make all required directories.
 #
 
-# Required directories.
-mkdir -v -p ~/bin ~/src ~/go/src/github.com
-
-# Directories for sources of different languages.
-declare -a src_dirs=(
-	"go"
-	"haskell"
-	"js"
-	"rust"
-	"scripts"
-	"third_party"
-	"websites"
-)
-
-for file in "${src_dirs[@]}"; do
-	mkdir -v -p "$HOME/src/$file"
-done
+# User specific binary directory.
+mkdir -v -p "$HOME/bin"
+# Some source related directories.
+mkdir -v -p "$HOME/src/notes"
+mkdir -v -p "$HOME/src/third_party"
+mkdir -v -p "$HOME/src/scripts"
+# Required for go.
+mkdir -v -p "$HOME/go/src/github.com"
 
 #
 # # Linking
 #
 
 # Link the directories required by go.
-ln -v -s ~/bin ~/go/bin
-ln -v -s ~/src/go ~/go/src/github.com/Thomasdezeeuw
-
-# Link all local files to the home directory.
-declare -a files_to_link=(
-	"bash_profile"
-	"editorconfig"
-	"npmrc"
-	"vimrc"
-)
-
-for file in "${files_to_link[@]}"; do
-	ln -v $file ~/.$file
-done
+ln -v -s "$HOME/bin" "$HOME/go/bin"
+ln -v -s "$HOME/src/go" "$HOME/go/src/github.com/Thomasdezeeuw"
 
 # Link all local files to the config directory.
 declare -a config_files=(
 	"git"
 	"i3"
 )
+
 for file in "${config_files[@]}"; do
-	mkdir -p ~/.config/$file
-	ln -v $file ~/.config/$file/config
+	mkdir -p "$HOME/.config/$file"
+	ln -v -L "$file" "$HOME/.config/$file/config"
 done
 
 echo "Required by install:
+
+Languages:
 	- Rust (and Cargo): https://www.rust-lang.org/en-US/install.html,
 	- Go: https://golang.org/dl,
-	- Gcc,
-	- Clang (llvm),
-	- Node.js.
+	- C: Gcc/Clang.
 
 Tools:
 	- Git,
 	- Exa 'cargo install exa',
 	- Ripgrep: 'cargo install ripgrep',
+	- fd: 'cargo install fd-find',
 	- Clippy: 'cargo install clippy',
 	- Vim,
 	- Vim plugins: https://github.com/junegunn/vim-plug,
@@ -72,7 +53,8 @@ Tools:
 	- Valgrind.
 
 SSH:
-	- Setup SSH server, including for public key only auth,
 	- Generate new keys, add to GitHub for easy pushing,
-	- Add local public to authorized keys,
-	- Check ~/.ssh/config."
+	- Check ~/.ssh/config.
+
+Misc:
+	- Fira Code font (https://github.com/tonsky/FiraCode)."
