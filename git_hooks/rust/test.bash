5f5382fc6aa3c869ab7aac27bebb510aee7c9b8c
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-08-06 14:25:07 +0200

    Expand testing in Rust testing Git hook

--- git_hooks/rust/test.bash
+++ git_hooks/rust/test.bash
@@ -7,6 +7,7 @@ echo "Testing"
 # Import `silent`.
 source "${BASH_SOURCE%/*}/../util.bash" || exit 1
 
-silent "Running tests" "cargo test"
+silent "Running tests" \
+	"cargo test --all --bins --tests --examples --benches --all-targets"
 
 exit 0
