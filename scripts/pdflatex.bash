8b1df1666c1fbc484ab13f8a89021fb57c209ac6
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-12-05 17:14:20 +0100

    Add pdflatex script

---
+++ scripts/pdflatex.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -u
+
+input=$1
+tmp_dir=$(mktemp -d)
+
+[ "${input: -4}" != ".tex" ] && echo "Expected .tex file" && exit 1
+
+base_name=$(basename -s .tex "$input")
+
+# Be quite when successful, loud otherwise (like Unix tools should).
+output=$(\latex -interaction=nonstopmode -halt-on-error -output-format=pdf \
+	-output-directory="$tmp_dir" "$input" 2>&1)
+
+if [ $? -ne 0 ]; then
+	echo "$output"
+	exit 1
+else
+	latex -interaction=nonstopmode -halt-on-error -output-format=pdf \
+		-output-directory="$tmp_dir" "$input" 2>&1 > /dev/null
+	mv "$tmp_dir/$base_name.pdf" .
+fi
