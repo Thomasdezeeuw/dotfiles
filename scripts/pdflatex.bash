65700a652c6595ec2de09121b5d38660d5248631
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-01-25 23:51:08 +0100

    Cleanup pdflatex script

--- scripts/pdflatex.bash
+++ scripts/pdflatex.bash
@@ -16,11 +16,13 @@ output=$(\latex -interaction=nonstopmode -halt-on-error -output-format=pdf \
 	-output-directory="$tmp_dir" "$input" 2>&1)
 ok=$?
 
-if [ $ok -ne 0 ]; then
-	echo "$output"
-	exit 1
-else
+if [ $ok -eq 0 ]; then
+	# Run latex again to fix reference, which often don't work after a single
+	# run.
 	latex -interaction=nonstopmode -halt-on-error -output-format=pdf \
 		-output-directory="$tmp_dir" "$input" > /dev/null 2>&1
 	mv "$tmp_dir/$base_name.pdf" .
+else
+	echo "$output"
+	exit 1
 fi
