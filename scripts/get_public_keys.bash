023d8766d3a4635cbb3e0b92cf428256d1f43610
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-01-26 13:58:27 +0100

    Add script to get GitHub user's public keys

---
+++ scripts/get_public_keys.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Get public keys for a GitHub user.
+#
+# Usage:
+# $ ./get_public_keys.bash >> ~/.ssh/authorized_keys
+
+GITHUB_USER=${1:-"Thomasdezeeuw"}
+
+echo "Getting $GITHUB_USER's key" >&2
+curl "https://github.com/$GITHUB_USER.keys"
