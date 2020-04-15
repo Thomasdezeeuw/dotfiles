7af7706900fc7c7529e532d480dc75b859a28d88
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-04-15 14:19:58 +0200

    Install Plug and TPM in server install script

--- install.server.bash
+++ install.server.bash
@@ -24,3 +24,15 @@ done
 ln -v -s "`pwd`/git" "$HOME/.gitconfig"
 mkdir -p "$HOME/.vim"
 ln -v -s "`pwd`/vimrc" "$HOME/.vim/vimrc"
+
+# Setup Vim-Plug.
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install all plugins.
+vim +PlugInstall +qall
+
+# Setup Tmux Plugin Manager.
+mkdir -r ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Install all plugins.
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
