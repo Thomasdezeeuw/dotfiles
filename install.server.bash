ce5cc361fdfa20cf12bd047107805efb88fc1a03
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-04-17 13:23:48 +0200

    Fix server install script

--- install.server.bash
+++ install.server.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 #
 # # Make all required directories.
 #
@@ -18,21 +20,22 @@ declare -a config_files=(
 	"tmux.conf"
 	"zprofile"
 )
+this_dir="$HOME/.dotfiles"
 for file in "${config_files[@]}"; do
-	ln -v -s "`pwd`/$file" "$HOME/.$file"
+	ln -v -s "$this_dir/$file" "$HOME/.$file"
 done
-ln -v -s "`pwd`/git" "$HOME/.gitconfig"
+ln -v -s "$this_dir/git" "$HOME/.gitconfig"
 mkdir -p "$HOME/.vim"
-ln -v -s "`pwd`/vimrc" "$HOME/.vim/vimrc"
+ln -v -s "$this_dir/vimrc" "$HOME/.vim/vimrc"
 
 # Setup Vim-Plug.
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Install all plugins.
 vim +PlugInstall +qall
 
 # Setup Tmux Plugin Manager.
-mkdir -r ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mkdir -p "$HOME/.tmux/plugins/tpm"
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 # Install all plugins.
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
+TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins/" "$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh"
